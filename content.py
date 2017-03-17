804414189deee10e87af52db7ac72b4ab123f62b
valera-chemtai <valeriachemtai28@gmail.com>
2017-03-17 06:48:50 +0300

    log_start and log_end functionalities added

--- content.py
+++ content.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, update
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from database import Students, Classes, Log_start, User
+from database import Students, Classes, Log, User
 
 #initiate link to the database for operation
 engine = create_engine("sqlite:///myproject_database.db")
@@ -21,7 +21,7 @@ class attendance_register:
 	#check_in method
 	def check_in(self):
 		#Check if student in database first
-		status = eval(input("Is student registered(True or False)? "))
+		status = eval(input("Is student registered(True or False)?  "))
 
 		if status == False:
 			#Take user to Add_student method with a prompt message
@@ -34,8 +34,8 @@ class attendance_register:
 			status = eval(input("Is student In another class(True or False)? "))
 
 			if status == False:
-				student_id = int(input("Enter student Id: "))
-				class_id = int(input("Enter class Id in mind: "))
+				student_id = int(input("Enter student Id:  "))
+				class_id = int(input("Enter class Id in mind:  "))
 				session.execute(update(Students).where(Students.id==student_id).values({"in_session":True, "class_id":class_id}))
 				session.commit()
 				print("Student Successfully assigned class")
@@ -47,10 +47,10 @@ class attendance_register:
 	def check_out(self):
 		student_id = int(input("Enter student Id: ")) #prompt for student Id
 
-		in_session = eval(input("Is student in session: (True or False)?: ")) #check if student is in session
+		in_session = eval(input("Is student in session: (True or False)?:  ")) #check if student is in session
 
 		if in_session == True:
-			check_out_reason = str(input("Enter reason for checking out: "))  #Reason for check out
+			check_out_reason = str(input("Enter reason for checking out:  "))  #Reason for check out
 
 			#change value of in_session in students table to False
 			session.execute(update(Students).where(Students.id==student_id).values({"in_session":False, "class_id":0}))
@@ -63,32 +63,46 @@ class attendance_register:
 
 	#method for initiating log_start
 	def log_start(self):
-		clas_id = int(input("Enter the class id: "))
-		class_in =Log_start(class_id=clas_id)
+		class_id = int(input("Enter the class id:  "))
+		class_in =Log(class_id=class_id)
 
-		start = eval(input("Enter start time: "))
-		starting = Log_start(class_id=clas_id, start_time=start)
+		start = str(input("Enter start time:  "))		
+		starting = Log(class_id=class_id, start_time=start)
 		session.add(starting)
 		session.commit()
 
+		log= []
+		rows = session.query(Log).all()
+		for row in rows:
+			log.append((row.class_id, row.start_time, row.end_time))
+		print(tabulate(log, headers=["class_id", "start_time", "end_time"], tablefmt="orgtbl"))
+
 	#method for log end
 	def log_end(self):
-		clas_id = int(input("Enter class id: "))
-		class_out = Log_start(class_id=clas_id)
+		class_id = int(input("Enter class id: "))
+		
+		end= str(input("Enter end time:  "))
+		
+		session.execute(update(Log).where(Log.class_id==class_id).values({"end_time":end}))
+		session.commit()
 
-		end= eval(input("Enter end time"))
-		ending = Log_end(class_id=clas_id, end_time=end)
+		log= []
+		rows = session.query(Log).all()
+		for row in rows:
+			log.append((row.class_id, row.start_time, row.end_time))
+		print(tabulate(log, headers=["class_id", "start_time", "end_time"], tablefmt="orgtbl"))
+
 
 	#Method to add student and automatically generate their id
 	def student_add(self):
-		new = input("Enter both first and surname separate with space: ")     #prompt user for both names separated by a space or one name
+		new = input("Enter both first and surname separate with space:  ")     #prompt user for both names separated by a space or one name
 		new_student = Students(student_name=new)
 
 		in_class = eval(input("Is student in class: (True or False): "))
 		student_session = Students(student_name=newStudent, in_session=in_class)			
 
 		if in_class == True:
-			class_id = int(input("Enter id of class in: "))
+			class_id = int(input("Enter id of class in:  "))
 			
 		else:
 			class_id = 0
@@ -109,13 +123,13 @@ class attendance_register:
 		new = input("Enter The Name of Class: ")
 		new_class = Classes(class_name=new)
 
-		class_on = eval(input("is the class in session(True or False): "))
+		class_on = eval(input("is the class in session(True or False):  "))
 		class_session = Classes(class_name=new, session_on=class_on)
 		session.add(class_session)
 		session.commit()
 
 	def class_remove(self):
-		class_id = int(input("Enter id in question: "))
+		class_id = int(input("Enter id in question:  "))
 		session.query(Classes).filter_by(id=class_id).delete()
 		session.commit()
 		
